191a7758d631434c62531e1e43be34e7d0ed3e9a
Krunal Shah <krunal8123@gmail.com>
2020-03-22 15:32:02 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CoronavirusComponent } from "./coronavirus/coronavirus.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "coronavirus", pathMatch: "full" },
+  { path: "coronavirus", component: CoronavirusComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
