f7abe8333c676d744889a7aa9894ce7ec1adc9cc
Krunal <krunal8123@gmail.com>
2020-03-22 21:46:57 +0530

    adding sub detail

--- src/app/coronavirus/coronavirus.component.ts
+++ src/app/coronavirus/coronavirus.component.ts
@@ -4,15 +4,24 @@ import { Corona } from "../models/corona";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material";
 import { Sort } from "@angular/material/sort";
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: "app-coronavirus",
   templateUrl: "./coronavirus.component.html",
-  styleUrls: ["./coronavirus.component.css"]
+  styleUrls: ["./coronavirus.component.css"],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class CoronavirusComponent implements OnInit {
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-
+  expandedElement: Corona;
+  selectedCountryName: string = "";
   displayedColumns = [
     "country_name",
     "cases",
@@ -27,7 +36,7 @@ export class CoronavirusComponent implements OnInit {
   dataLoading: boolean = true;
   dataSource: any;
   sortedData: any;
-  constructor(private Covid19Service: Covid19Service) {}
+  constructor(private Covid19Service: Covid19Service) { }
 
   ngOnInit() {
     this.GetCasesByCountry();
@@ -91,15 +100,27 @@ export class CoronavirusComponent implements OnInit {
           item.deaths.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
           item.new_deaths.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
           item.total_recovered.toLowerCase().indexOf(value.toLowerCase()) >
-            -1 ||
+          -1 ||
           item.active_cases.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
           item.serious_critical.toLowerCase().indexOf(value.toLowerCase()) >
-            -1 ||
+          -1 ||
           item.total_cases_per_1m_population
             .toLowerCase()
             .indexOf(value.toLowerCase()) > -1
       );
     }
+  }
+
+  setSelectedCountryName(element: Corona) {
+
+    this.expandedElement = element;
+    this.selectedCountryName = element.country_name;
+  }
+
+  removeSelectedCountryName() {
+
+    this.expandedElement = undefined;
+    this.selectedCountryName = "";
   }
 }
 function compare(a: number | string, b: number | string, isAsc: boolean) {
