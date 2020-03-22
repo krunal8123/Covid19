cd15da1f20c362b5ada3064ce7ca12f8d8f99bc1
Krunal Shah <krunal8123@gmail.com>
2020-03-22 14:39:15 +0530

    error solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  constructor(private _configs: ConfigService) {}
+  constructor() {}
   displayedColumns = [
     "Country",
     "position",
