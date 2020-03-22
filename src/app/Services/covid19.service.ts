99ad556b2484d63e566188b706296fdce9545b55
Krunal Shah <krunal8123@gmail.com>
2020-03-22 16:13:42 +0530

    coronavirus component

--- src/app/Services/covid19.service.ts
+++ src/app/Services/covid19.service.ts
@@ -1,64 +1,86 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams, HttpBackend } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpBackend
+} from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { Corona } from "../models/corona";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class Covid19Service {
-
   api: string = environment.API;
   X_RAPIDAPI_HOST: string = environment.X_RAPIDAPI_HOST;
   X_RAPIDAPI_KEY: string = environment.X_RAPIDAPI_KEY;
 
-  constructor(private HttpClient: HttpClient) { }
+  constructor(private HttpClient: HttpClient) {}
 
-  async GetCasesByCountry() {
-
-    let header = new HttpHeaders().set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com").
-      set("x-rapidapi-key", "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e");
-
-    this.HttpClient.get(this.api + "cases_by_country.php", { headers: header }).subscribe(async (data: any) => {
-      console.log(data);
-      return await data;
-    })
+  GetCasesByCountry() {
+    let header = new HttpHeaders()
+      .set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com")
+      .set(
+        "x-rapidapi-key",
+        "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e"
+      );
+    return this.HttpClient.get(this.api + "cases_by_country.php", {
+      headers: header
+    });
   }
 
   async GetAffectedCountries() {
+    let header = new HttpHeaders()
+      .set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com")
+      .set(
+        "x-rapidapi-key",
+        "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e"
+      );
 
-    let header = new HttpHeaders().set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com").
-      set("x-rapidapi-key", "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e");
-
-    this.HttpClient.get(this.api + "affected.php", { headers: header }).subscribe(async (data: any) => {
+    this.HttpClient.get(this.api + "affected.php", {
+      headers: header
+    }).subscribe(async (data: any) => {
       console.log(data);
       return await data;
-    })
+    });
   }
 
   async GetHistoryByParticularCountry(CountryName: string) {
-
     let param: HttpParams = new HttpParams().set("country", CountryName);
 
-    let header = new HttpHeaders().set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com").
-      set("x-rapidapi-key", "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e");
+    let header = new HttpHeaders()
+      .set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com")
+      .set(
+        "x-rapidapi-key",
+        "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e"
+      );
 
-    this.HttpClient.get(this.api + "cases_by_particular_country.php", { headers: header, params: param }).subscribe(async (data: any) => {
+    this.HttpClient.get(this.api + "cases_by_particular_country.php", {
+      headers: header,
+      params: param
+    }).subscribe(async (data: any) => {
       console.log(data);
       return await data;
-    })
+    });
   }
 
   async GetLatestStatusByCountry(CountryName: string) {
-
     let param: HttpParams = new HttpParams().set("country", CountryName);
 
-    let header = new HttpHeaders().set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com").
-      set("x-rapidapi-key", "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e");
+    let header = new HttpHeaders()
+      .set("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com")
+      .set(
+        "x-rapidapi-key",
+        "cfe2855d40mshb45703d05279605p189c97jsnc368394faf8e"
+      );
 
-    this.HttpClient.get(this.api + "latest_stat_by_country.php", { headers: header, params: param }).subscribe(async (data: any) => {
+    this.HttpClient.get(this.api + "latest_stat_by_country.php", {
+      headers: header,
+      params: param
+    }).subscribe(async (data: any) => {
       console.log(data);
       return await data;
-    })
+    });
   }
 }
--- src/app/coronavirus/coronavirus.component.ts
+++ src/app/coronavirus/coronavirus.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Covid19Service } from "../Services/covid19.service";
+import { Corona } from "../models/corona";
 @Component({
   selector: "app-coronavirus",
   templateUrl: "./coronavirus.component.html",
@@ -7,53 +8,27 @@ import { Covid19Service } from "../Services/covid19.service";
 })
 export class CoronavirusComponent implements OnInit {
   displayedColumns = [
-    "Country",
-    "position",
-    "weight",
-    "symbol",
-    "position",
-    "weight",
-    "symbol",
-    "star"
+    "country_name",
+    "cases",
+    "new_cases",
+    "deaths",
+    "new_deaths",
+    "total_recovered",
+    "active_cases",
+    "serious_critical",
+    "total_cases_per_1m_population"
   ];
-  dataSource = ELEMENT_DATA;
+  dataSource: Corona[] = [];
   constructor(private Covid19Service: Covid19Service) {}
 
   ngOnInit() {
     this.GetCasesByCountry();
   }
-  async GetCasesByCountry() {
-    let Data = await this.Covid19Service.GetCasesByCountry();
+  GetCasesByCountry() {
+    this.Covid19Service.GetCasesByCountry().subscribe((data: any) => {
+      this.dataSource = data.countries_stat.map(result =>
+        Object.assign(new Corona(), result)
+      );
+    });
   }
 }
-export interface coronaData {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: coronaData[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
-];
