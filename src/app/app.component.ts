a9d4302a00e5847cca7fba7d51ed970101993072
Krunal Shah <krunal8123@gmail.com>
2020-03-22 14:21:01 +0530

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
-
+import { ConfigService } from "../app/config.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  constructor(private _configs: ConfigService) {}
   displayedColumns = [
     "name",
     "position",
