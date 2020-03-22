8f7dcf22c131a04ded3c582f0db03e72d0bf58a7
Krunal <krunal8123@gmail.com>
2020-03-22 17:47:20 +0530

    adding history

---
+++ src/app/models/history.ts
@@ -0,0 +1,15 @@
+export class History {
+    id: string;
+    country_name: string;
+    total_cases: number;
+    new_cases: number;
+    active_cases: number;
+    total_deaths: number;
+    new_deaths: number;
+    total_recovered: number;
+    serious_critical: number;
+    region: string;
+    total_cases_per1m: string;
+    record_date: string;
+
+}
