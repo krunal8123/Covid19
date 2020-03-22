e89706dd43ea01c8b08732c021280c0d09a5ac51
Krunal Shah <krunal8123@gmail.com>
2020-03-22 23:14:12 +0530

    charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,29 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material/material.module";
 import { HttpClientModule } from "@angular/common/http";
-import { CoronavirusComponent } from './coronavirus/coronavirus.component';
-import { HistoryComponent } from './history/history.component';
-import { LatestStatusComponent } from './latest-status/latest-status.component';
-import { LatestStatusByCountryComponent } from './latest-status-by-country/latest-status-by-country.component';
-import { Codiv19SubDetailComponent } from './codiv19-sub-detail/codiv19-sub-detail.component';
+import { CoronavirusComponent } from "./coronavirus/coronavirus.component";
+import { HistoryComponent } from "./history/history.component";
+import { LatestStatusComponent } from "./latest-status/latest-status.component";
+import { LatestStatusByCountryComponent } from "./latest-status-by-country/latest-status-by-country.component";
+import { Codiv19SubDetailComponent } from "./codiv19-sub-detail/codiv19-sub-detail.component";
+import { ChartsModule } from "ng2-charts";
 
 @NgModule({
-  declarations: [AppComponent, CoronavirusComponent, HistoryComponent, LatestStatusComponent, LatestStatusByCountryComponent, Codiv19SubDetailComponent],
+  declarations: [
+    AppComponent,
+    CoronavirusComponent,
+    HistoryComponent,
+    LatestStatusComponent,
+    LatestStatusByCountryComponent,
+    Codiv19SubDetailComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,48 +1,123 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { Covid19Service } from '../Services/covid19.service';
-import { History } from '../models/history';
-import { error } from '@angular/compiler/src/util';
+import { Component, OnInit, Input, Output } from "@angular/core";
+import { Covid19Service } from "../Services/covid19.service";
+import { History } from "../models/history";
+import { ChartDataSets, ChartOptions } from "chart.js";
+import { Color, Label } from "ng2-charts";
 
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  selector: "app-history",
+  templateUrl: "./history.component.html",
+  styleUrls: ["./history.component.css"]
 })
 export class HistoryComponent implements OnInit {
-
   @Input() countryName: string = "";
   @Input() CurrentCountryName: string = "";
   dataLoading: boolean = false;
 
   historyData: History[] = [];
 
-  constructor(private Covid19Service: Covid19Service) { }
+  /////////////////////////////////////////////////////
+  public lineChartData: ChartDataSets[] = [];
+  public lineChartLabels: Label[] = [];
+  public lineChartOptions: ChartOptions = {
+    responsive: true
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: "black",
+      backgroundColor: "rgba(255,0,0,0.3)"
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType = "line";
+  public lineChartPlugins = [];
+  /////////////////////////////////////////////////////
+
+  constructor(private Covid19Service: Covid19Service) {
+    this.GetHistoryByParticularCountry();
+  }
 
   ngOnChanges(changes) {
-
-    if (this.countryName != this.CurrentCountryName &&
-      this.countryName.length > 0) {
-
+    if (
+      this.countryName != this.CurrentCountryName &&
+      this.countryName.length > 0
+    ) {
       this.GetHistoryByParticularCountry();
     }
   }
 
   GetHistoryByParticularCountry() {
-
     this.dataLoading = true;
-    this.Covid19Service.GetHistoryByParticularCountry(this.countryName).subscribe((data: any) => {
-
-      this.dataLoading = false;
-      this.historyData = data.stat_by_country.map(result =>
-        Object.assign(new History(), result)
-      );
-    }, (error) => {
-      this.dataLoading = false;
-    });
+    this.Covid19Service.GetHistoryByParticularCountry(
+      this.countryName
+    ).subscribe(
+      (data: any) => {
+        this.dataLoading = false;
+        this.historyData = data.stat_by_country.map(result =>
+          Object.assign(new History(), result)
+        );
+        console.log(this.historyData);
+        if (this.historyData.length > 0) {
+          // let firstRecord = new Date(
+          //   this.historyData[0].record_date
+          // ).toLocaleDateString("fr-CA");
+          // let lastRecord = new Date(
+          //   this.historyData[this.historyData.length - 1].record_date
+          // ).toLocaleDateString("fr-CA");
+          let newCases = [];
+          let totalCases = [];
+          let activeCases = [];
+          let totalDeaths = [];
+          let newDeaths = [];
+          let totalRecovered = [];
+          this.historyData.forEach(item => {
+            this.lineChartLabels.push(item.record_date);
+            newCases.push(item.new_cases);
+            totalCases.push(item.total_cases);
+            activeCases.push(item.active_cases);
+            totalDeaths.push(item.total_deaths);
+            newDeaths.push(item.new_deaths);
+            totalRecovered.push(item.total_recovered);
+          });
+          let data = newCases;
+          let totalcasesData = totalCases;
+          let activeCasesData = activeCases;
+          let totalDeathsData = totalDeaths;
+          let newDeathsData = newDeaths;
+          let totalRecoveredData = totalRecovered;
+          this.lineChartData.push(
+            {
+              data: data,
+              label: "New Cases for - " + this.countryName
+            },
+            {
+              data: totalcasesData,
+              label: "Total Cases for - " + this.countryName
+            },
+            {
+              data: activeCasesData,
+              label: "Active Cases for - " + this.countryName
+            },
+            {
+              data: totalDeathsData,
+              label: "Total Death Cases for - " + this.countryName
+            },
+            {
+              data: newDeathsData,
+              label: "New Deaths in - " + this.countryName
+            },
+            {
+              data: totalRecoveredData,
+              label: "Total Recovered Cases for - " + this.countryName
+            }
+          );
+        }
+      },
+      error => {
+        this.dataLoading = false;
+      }
+    );
   }
 
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 }
