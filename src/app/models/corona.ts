a19dacf05831f56ba4d762820627a249561df959
Krunal Shah <krunal8123@gmail.com>
2020-03-22 15:38:18 +0530

    update

--- src/app/models/corona.ts
+++ src/app/models/corona.ts
@@ -1,2 +1,12 @@
 export class Corona {
+  country_name: string;
+  cases: String;
+  deaths: String;
+  region: String;
+  total_recovered: String;
+  new_deaths: String;
+  new_cases: String;
+  serious_critical: String;
+  active_cases: String;
+  total_cases_per_1m_population: String;
 }
